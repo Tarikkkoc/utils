ccd3f679b0103edc1c22a0b900d43f05af695000
Tarikkoc <ahmettarikkoc@gmail.com>
2023-08-06 10:22:17 +0300

    editted

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-var numbers = [1, [2, [3, [4]], 5]];
-var newArr = _.flatten(numbers);
+function flatten(array) {
+  var newArr = [];
+  if (Array.isArray(array)) {
+    array.map((a) => {
+      if (Array.isArray(a)) {
+        const values = a.values();
+        // console.log(values.next().value);
+        newArr.push(values.next().value);
+      } else {
+        newArr.push(a);
+      }
+    });
+  } else {
+    return newArr;
+  }
+  return newArr;
+}
 
-console.log(newArr);
+const numbers = [1, [2], 5];
+console.log(flatten(numbers));
