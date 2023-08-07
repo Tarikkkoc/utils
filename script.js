ac359a4b751910576777d31536f235b6b41567c5
Tarikkoc <ahmettarikkoc@gmail.com>
2023-08-07 15:28:50 +0300

    added shuffle function

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ function flatten(array) {
   if (Array.isArray(array)) {
     array.map((a) => {
       if (Array.isArray(a)) {
-        // newArr = newArr.concat(flatten(a));
-        // const values = a.values();
-        // newArr.push(values.next().value);
         a.forEach((b) => {
           newArr.push(b);
         });
@@ -18,6 +15,16 @@ function flatten(array) {
   }
   return newArr;
 }
-
 const numbers = [1, [2, [3, 4, [7]]], 5];
 console.log(flatten(numbers));
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    var random = Math.floor(Math.random() * (i + 1));
+    [array[i], array[random]] = [array[random], array[i]];
+  }
+  return array;
+}
+
+const words = ["pen", "car", "furniture", "bed"];
+console.log(shuffle(words));
